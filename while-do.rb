41c6e09c8863154a88eee8987a8b02fda172fc89
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-25 07:00:39 +0000

    Automatically backed up by Learn

---
+++ while-do.rb
@@ -0,0 +1,22 @@
+=begin
+count = 0
+while count < 3 do
+  puts "I am the #{count}, I love to count!"
+  count += 1
+end
+=end
+
+
+=begin
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!"
+  count += 1 
+end
+=end
+
+
+3.times do 
+  puts "I ran."
+end
